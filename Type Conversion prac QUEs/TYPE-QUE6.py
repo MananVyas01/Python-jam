9f9c054e772ab8793964c5310a8a540063228060
MananVyas01 <vyasmanan2511@gmail.com>
2024-06-24 14:31:45 +0530

    new que is add

---
+++ Type Conversion prac QUEs/TYPE-QUE6.py
@@ -0,0 +1,9 @@
+#Convert the string “Nividata consultancy” to a set
+
+text = "Nividata consultancy"
+
+set_text = set(text)
+
+'''we already know that it will remore the duplicated item and give it in unordered list'''
+
+print(set_text)
